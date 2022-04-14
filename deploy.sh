7c1afaed648ef8ff599f84d59a09e3a6bb5ae105
Senna Parsa <sennap@github.com>
2022-04-14 15:44:56 +0000

    variables

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 # *nix only
 export RG="reddog"
 export LOCATION="eastus2"
-export SUB_ID="<YourSubscriptionID>"
+export SUB_ID="<id>"
 
 # Follow Azure CLI prompts to authenticate to your subscription of choice
-az login
+az login 
 az account set --subscription $SUB_ID
 
 # Create resource group
-az group create -n $RG -l $LOCATION
+az group create -n reddog -l eastus
 
 # Deploy all infrastructure and reddog apps
-az deployment group create -n reddog -g $RG -f ./deploy/bicep/main.bicep
+az deployment group create -n reddog -g reddog -f ./deploy/bicep/main.bicep
 
 # Show outputs for bicep deployment
-az deployment group show -n reddog -g $RG -o json --query properties.outputs.urls.value
+az deployment group show -n reddog -g reddog -o json --query properties.outputs.urls.value
